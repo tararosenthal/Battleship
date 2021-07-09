4def7f9361134a36a3250a53a344f64944cfbd7e
tararosenthal <65468485+tararosenthal@users.noreply.github.com>
2021-07-09 12:32:26 -0500

    Create CoordinatesType.java

---
+++ model/enums/CoordinatesType.java
@@ -0,0 +1,11 @@
+package battleship.model.enums;
+
+/*
+ * Defines type of coordinates. For use with CoordinateArray objects including ShipCoordinatesArray and ShotCoordinatesArray. Invalid used with validating input methods only.
+ */
+
+public enum CoordinatesType {
+    SHIP,
+    SHOT,
+    INVALID
+}
